69c5914e07bd918e2e6b6db92f6025170f3c203a
ukorp <ukorp63@gmail.com>
2025-01-29 22:12:36 +0300

    feat: docker compose

--- frontend/src/router/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {Navigate} from "react-router-dom";
-import isTokenExpired from "../auth/isTokenExpired";
-
-const PrivateRoute = ({children}) => {
-    return (!isTokenExpired()) ? children : <Navigate to="/login"/>;
-}
-
-export default PrivateRoute
